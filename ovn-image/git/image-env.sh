95cad6c086f78b5ff4d88e933f82c9d50a2fc534
Matthew Booth <mbooth@redhat.com>
2020-09-28 22:35:35 +0100

    ovn-image: Add cluster-kick and use common scripts

---
+++ ovn-image/git/image-env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+PATH=/opt/bin:/opt/sbin:${PATH}
+OVN_LIBDIR="/opt/share/ovn"
--- ovn-image/scripts-common/env.sh
+++ ovn-image/scripts-common/env.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-PATH=/opt/bin:/opt/sbin:${PATH}
+. /image-env.sh
+
+OVN_SCRIPTSDIR=${OVN_SCRIPTSDIR:-${OVN_LIBDIR}/scripts}
 
 if [ "${DB_TYPE}" == "NB" ]; then
-    db="${OVN_DBDIR}/ovnnb_db.db"
+    lower="nb"
     db_name="OVN_Northbound"
-    db_sock=${OVN_RUNDIR}/ovnnb_db.sock
-    db_ctl=${OVN_RUNDIR}/ovnnb_db.ctl
     db_port=6641
     raft_port=6643
     db_global_table=NB_Global
-    schema="/opt/share/ovn/ovn-nb.ovsschema"
 elif [ "${DB_TYPE}" == "SB" ]; then
-    db="${OVN_DBDIR}/ovnsb_db.db"
     db_name="OVN_Southbound"
-    db_sock=${OVN_RUNDIR}/ovnsb_db.sock
-    db_ctl=${OVN_RUNDIR}/ovnsb_db.ctl
     db_port=6642
     raft_port=6644
     db_global_table=SB_Global
-    schema="/opt/share/ovn/ovn-sb.ovsschema"
 else
     echo "Unknown DB_TYPE: ${DB_TYPE}" >&2
     exit 1
 fi
+
+db="${OVN_DBDIR}/ovn${lower}_db.db"
+db_sock=${OVN_RUNDIR}/ovn${lower}_db.sock
+db_ctl=${OVN_RUNDIR}/ovn${lower}_db.ctl
+schema=${OVN_LIBDIR}/ovn-${lower}.ovsschema
 
 if [ -z "${SERVER_NAME}" ]; then
     echo "SERVER_NAME is not set" >&2
@@ -31,4 +31,4 @@ if [ -z "${SERVER_NAME}" ]; then
 fi
 
 raft_address=tcp:${SERVER_NAME}:$raft_port
-ovn_ctl=/opt/share/ovn/scripts/ovn-ctl
+ovn_ctl=${OVN_SCRIPTSDIR}/ovn-ctl
