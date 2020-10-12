244a19c0689258c34d96457c6bdc6b5b6f5a1f71
Matthew Booth <mbooth@redhat.com>
2020-10-12 09:55:25 +0100

    Move ovn-image to its own repo
    Moved to https://github.com/openstack-k8s-operators/ovn-image

--- ovn-image/git/image-env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-PATH=/opt/bin:/opt/sbin:${PATH}
-OVN_LIBDIR="/opt/share/ovn"
--- ovn-image/pkg/image-env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-OVN_LIBDIR="/usr/share/openvswitch"
-
-# These environment variables changed name at some point. OVN_* is the new name, but this version
-# uses OVS_*.
-export OVS_DBDIR=${OVN_DBDIR}
-export OVS_RUNDIR=${OVN_RUNDIR}
