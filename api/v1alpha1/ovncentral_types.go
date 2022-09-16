07a8ba324e58c107a63f242fe621f68eb226979f
Yatin Karel <ykarel@redhat.com>
2022-09-16 14:41:09 +0000

    Add resource/controllers using sdk

---
+++ api/v1alpha1/ovncentral_types.go
@@ -0,0 +1,64 @@
+/*
+Copyright 2022.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// OVNCentralSpec defines the desired state of OVNCentral
+type OVNCentralSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// Foo is an example field of OVNCentral. Edit ovncentral_types.go to remove/update
+	Foo string `json:"foo,omitempty"`
+}
+
+// OVNCentralStatus defines the observed state of OVNCentral
+type OVNCentralStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// OVNCentral is the Schema for the ovncentrals API
+type OVNCentral struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   OVNCentralSpec   `json:"spec,omitempty"`
+	Status OVNCentralStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// OVNCentralList contains a list of OVNCentral
+type OVNCentralList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []OVNCentral `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&OVNCentral{}, &OVNCentralList{})
+}
