2f91d95980966a8f24c6737d0e35701d4bc5348e
Yatin Karel <ykarel@redhat.com>
2022-12-13 05:57:51 +0000

    Logging: Use ISO8601 timestamps
    The default value for the zap logger is using epoch timestamps, but
    that's not user friendly, so we are changing it to using
    the ISO8601 time format instead.
    
    The zap package from uber is already a dependency from the zap module
    of the controller-runtime package we already use, so we are not
    adding any new dependency.
    
    Signed-off-by: Yatin Karel <ykarel@redhat.com>

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ import (
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	"go.uber.org/zap/zapcore"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -61,6 +62,7 @@ func main() {
 			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
 		Development: true,
+		TimeEncoder: zapcore.ISO8601TimeEncoder,
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
