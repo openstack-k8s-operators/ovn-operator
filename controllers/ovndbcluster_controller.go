5d19cf63102f30a270301083533eaf19b53a7add
Takashi Kajinami <tkajinam@redhat.com>
2023-02-10 13:49:54 +0900

    Return error in ConfigMap creation
    The current logic ignores the error but it should be returned so that
    the error can be detected in the reconciler.

--- controllers/ovndbcluster_controller.go
+++ controllers/ovndbcluster_controller.go
@@ -467,12 +467,7 @@ func (r *OVNDBClusterReconciler) generateServiceConfigMaps(
 			ConfigOptions: templateParameters,
 		},
 	}
-	err := configmap.EnsureConfigMaps(ctx, h, instance, cms, envVars)
-	if err != nil {
-		return nil
-	}
-
-	return nil
+	return configmap.EnsureConfigMaps(ctx, h, instance, cms, envVars)
 }
 
 // createHashOfInputHashes - creates a hash of hashes which gets added to the resources which requires a restart
