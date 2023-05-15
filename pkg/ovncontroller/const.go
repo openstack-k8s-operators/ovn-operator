3e6da86c0f3501037e22bcdd1f2cc724cf50123c
Ihar Hrachyshka <ihrachys@redhat.com>
2023-05-15 21:26:49 +0000

    Copy pkg/ovs from ovs-operator to pkg/ovncontroller

---
+++ pkg/ovncontroller/const.go
@@ -0,0 +1,12 @@
+package ovncontroller
+
+const (
+	// ServiceName - ovs service name
+	ServiceName = "ovs"
+	// OvnControllerServiceName - ovn-controller service name
+	OvnControllerServiceName = "ovn-controller"
+	// ServiceType -
+	ServiceType = "ovs"
+	// KollaConfigAPI -
+	KollaConfigAPI = "/var/lib/config-data/merged/ovs-config.json"
+)
