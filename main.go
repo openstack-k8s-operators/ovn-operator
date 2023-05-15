5eb71678da1aedb19f3f661dcc1fe5dab4311a09
Ihar Hrachyshka <ihrachys@redhat.com>
2023-05-15 20:45:02 +0000

    move OVNControllerReconciler up

--- main.go
+++ main.go
@@ -116,6 +116,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "OVNDBCluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.OVNControllerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OVNController")
+		os.Exit(1)
+	}
 
 	// Acquire environmental defaults and initialize OVNDBCluster defaults with them
 	ovnDbClusterDefaults := ovnv1.OVNDBClusterDefaults{
@@ -144,13 +151,6 @@ func main() {
 		}
 	}
 
-	if err = (&controllers.OVNControllerReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "OVNController")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
