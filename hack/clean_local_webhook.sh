6a5cd012ccf8c874057c1e645c415f7fce117fd3
Ihar Hrachyshka <ihrachys@redhat.com>
2023-05-16 19:41:22 +0000

    Clean up new webhook

--- hack/clean_local_webhook.sh
+++ hack/clean_local_webhook.sh
@@ -5,3 +5,5 @@ oc delete validatingwebhookconfiguration/vovndbcluster.kb.io --ignore-not-found
 oc delete mutatingwebhookconfiguration/movndbcluster.kb.io --ignore-not-found
 oc delete validatingwebhookconfiguration/vovnnorthd.kb.io --ignore-not-found
 oc delete mutatingwebhookconfiguration/movnnorthd.kb.io --ignore-not-found
+oc delete validatingwebhookconfiguration/vovncontroller.kb.io --ignore-not-found
+oc delete mutatingwebhookconfiguration/movncontroller.kb.io --ignore-not-found
