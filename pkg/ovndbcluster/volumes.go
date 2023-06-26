41452c51b9505fcc4b7a30028f598f61686e3a8f
Ihar Hrachyshka <ihrachys@redhat.com>
2023-06-26 20:34:19 +0000

    Remove unnecessary hostPath volumes for localtime and machine-id
    Northd and ovndbcluster pods have no need for tight coupling to a node and
    should be completely safe to relocate to a different node as needed.
    
    Having such volumes is specifically a problem when it comes to removing SCC
    that allows host mounts, which requires escalated access.

--- pkg/ovndbcluster/volumes.go
+++ pkg/ovndbcluster/volumes.go
@@ -11,22 +11,6 @@ func GetDBClusterVolumes(name string) []corev1.Volume {
 
 	return []corev1.Volume{
 		{
-			Name: "etc-machine-id",
-			VolumeSource: corev1.VolumeSource{
-				HostPath: &corev1.HostPathVolumeSource{
-					Path: "/etc/machine-id",
-				},
-			},
-		},
-		{
-			Name: "etc-localtime",
-			VolumeSource: corev1.VolumeSource{
-				HostPath: &corev1.HostPathVolumeSource{
-					Path: "/etc/localtime",
-				},
-			},
-		},
-		{
 			Name: "scripts",
 			VolumeSource: corev1.VolumeSource{
 				ConfigMap: &corev1.ConfigMapVolumeSource{
@@ -55,16 +39,6 @@ func GetDBClusterVolumes(name string) []corev1.Volume {
 // GetDBClusterVolumeMounts - OVN DBCluster VolumeMounts
 func GetDBClusterVolumeMounts(name string) []corev1.VolumeMount {
 	return []corev1.VolumeMount{
-		{
-			Name:      "etc-machine-id",
-			MountPath: "/etc/machine-id",
-			ReadOnly:  true,
-		},
-		{
-			Name:      "etc-localtime",
-			MountPath: "/etc/localtime",
-			ReadOnly:  true,
-		},
 		{
 			Name:      "scripts",
 			MountPath: "/usr/local/bin/container-scripts",
--- pkg/ovnnorthd/volumes.go
+++ pkg/ovnnorthd/volumes.go
@@ -10,22 +10,6 @@ func GetNorthdVolumes(name string) []corev1.Volume {
 
 	return []corev1.Volume{
 		{
-			Name: "etc-machine-id",
-			VolumeSource: corev1.VolumeSource{
-				HostPath: &corev1.HostPathVolumeSource{
-					Path: "/etc/machine-id",
-				},
-			},
-		},
-		{
-			Name: "etc-localtime",
-			VolumeSource: corev1.VolumeSource{
-				HostPath: &corev1.HostPathVolumeSource{
-					Path: "/etc/localtime",
-				},
-			},
-		},
-		{
 			Name: "config-data",
 			VolumeSource: corev1.VolumeSource{
 				ConfigMap: &corev1.ConfigMapVolumeSource{
@@ -43,16 +27,6 @@ func GetNorthdVolumes(name string) []corev1.Volume {
 // GetNorthdVolumeMounts - OVN Northd VolumeMounts
 func GetNorthdVolumeMounts() []corev1.VolumeMount {
 	return []corev1.VolumeMount{
-		{
-			Name:      "etc-machine-id",
-			MountPath: "/etc/machine-id",
-			ReadOnly:  true,
-		},
-		{
-			Name:      "etc-localtime",
-			MountPath: "/etc/localtime",
-			ReadOnly:  true,
-		},
 		{
 			Name:      "config-data",
 			MountPath: "/var/lib/config-data",
