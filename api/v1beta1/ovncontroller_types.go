c9259d71750bf4a2ad000d2771bbe318e5102804
Ihar Hrachyshka <ihrachys@redhat.com>
2024-03-25 23:00:07 +0000

    Fix description for ovsNumberReady
    Remove dash; also remove unneeded details.

--- api/v1beta1/ovncontroller_types.go
+++ api/v1beta1/ovncontroller_types.go
@@ -97,7 +97,7 @@ type OVNControllerStatus struct {
 	// NumberReady of the OVNController instances
 	NumberReady int32 `json:"numberReady,omitempty"`
 
-	// OVS-NumberReady of the ovs-vswitchd + ovsdb-server instances
+	// ovsNumberReady of ovs instances
 	OVSNumberReady int32 `json:"ovsNumberReady,omitempty"`
 
 	// DesiredNumberScheduled - total number of the nodes which should be running Daemon
