303cbcdcb1cfa9d658a35dd927da4cb47e9df79f
Elvira García <egarciar@redhat.com>
2024-06-10 12:37:04 +0200

    Fix gateway datapath disrupt on update
    Reimplementing the vswitchd reload using separate start and stop scripts
    so that it can be executed partially between consecutives instances of
    the pod. To avoid disruptions, it runs ovs save-flows command on the
    stop-vswitchd script, saving the resulting backup on a mounted folder
    from the host and then loading that backup on the start script. For this
    to succeed, the script needs to set the flow-restore-wait flag from ovs
    to true while loading the flows.
    
    Note that ovs save-flows command needs to have ovsdb-server running, so
    a semaphore like mechanism was added to ensure the ovsdb-server
    container is never deleted before ovs-vswitchd.
    
    Closes-Issue: OSPRH-6326

---
+++ templates/ovncontroller/bin/stop-vswitchd.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# Copyright 2024 Red Hat Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+set -ex
+source $(dirname $0)/functions
+
+# Clean up any previously created flow backups to avoid conflict with newly
+# generated backup.
+cleanup_flows_backup
+
+# Passing --real to mimic what upstream startup scripts do; maybe redundant.
+bridges=$(ovs-vsctl -- --real list-br)
+
+# Saving flows to avoid disrupting gateway datapath.
+mkdir $FLOWS_RESTORE_DIR
+TMPDIR=$FLOWS_RESTORE_DIR /usr/share/openvswitch/scripts/ovs-save save-flows $bridges > $FLOWS_RESTORE_SCRIPT
+
+# Once save-flows logic is complete it no longer needs ovsdb-server, this file
+# unlocks the db preStop script, working as a semaphore
+touch $SAFE_TO_STOP_OVSDB_SERVER_SEMAPHORE
+
+# Finally, stop vswitchd.
+/usr/share/openvswitch/scripts/ovs-ctl stop --no-ovsdb-server
