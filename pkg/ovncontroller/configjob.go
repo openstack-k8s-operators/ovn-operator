c87f6b180d1c9db119502591668f15568a3fd7f4
Martin Schuppert <mschuppert@redhat.com>
2025-04-16 16:09:19 +0200

    fix golangci reported issues
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- pkg/ovncontroller/configjob.go
+++ pkg/ovncontroller/configjob.go
@@ -10,6 +10,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Package ovncontroller provides functionality for managing OVN controller components
 package ovncontroller
 
 import (
--- pkg/ovndbcluster/const.go
+++ pkg/ovndbcluster/const.go
@@ -1,11 +1,14 @@
+// Package ovndbcluster provides functionality for managing OVN database clusters
 package ovndbcluster
 
 const (
-	// ServiceNameNB -
-	DbPortNB   int32 = 6641
+	// DbPortNB is the port number for the OVN Northbound database
+	DbPortNB int32 = 6641
+	// RaftPortNB is the port number for the OVN Northbound database Raft protocol
 	RaftPortNB int32 = 6643
 
-	// ServiceNameSB -
-	DbPortSB   int32 = 6642
+	// DbPortSB is the port number for the OVN Southbound database
+	DbPortSB int32 = 6642
+	// RaftPortSB is the port number for the OVN Southbound database Raft protocol
 	RaftPortSB int32 = 6644
 )
