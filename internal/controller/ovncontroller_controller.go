dcf9eddd9bae6cf08e8164abe467dba9228e1d58
Martin Schuppert <mschuppert@redhat.com>
2026-07-24 15:18:49 +0200

    Remove unnecessary pods RBAC permissions
    The workload rbacRules and kubebuilder RBAC markers granted full CRUD
    (create/delete/get/list/patch/update/watch) on core Pods, which was never
    exercised. Remove the unused permission and regenerate config/rbac/role.yaml.
    
    The narrower get;list markers on ovncontroller, ovndbcluster and ovnnorthd
    are kept, since those controllers list pods by label to track DB cluster
    and controller pod status.
    
    Signed-off-by: Martin Schuppert <mschuppert@redhat.com>

--- internal/controller/ovncontroller_controller.go
+++ internal/controller/ovncontroller_controller.go
@@ -94,7 +94,6 @@ func (r *OVNControllerReconciler) GetClient() client.Client {
 // +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=rolebindings,verbs=get;list;watch;create;update;patch
 // service account permissions that are needed to grant permission to the above
 // +kubebuilder:rbac:groups="security.openshift.io",resourceNames=anyuid;privileged,resources=securitycontextconstraints,verbs=use
-// +kubebuilder:rbac:groups="",resources=pods,verbs=create;delete;get;list;patch;update;watch
 // +kubebuilder:rbac:groups=topology.openstack.org,resources=topologies,verbs=get;list;watch;update
 
 // Reconcile reconciles the OVNController CR to deploy OVN controller pods
@@ -377,11 +376,6 @@ func (r *OVNControllerReconciler) reconcileNormal(ctx context.Context, instance 
 			ResourceNames: []string{"anyuid", "privileged"},
 			Resources:     []string{"securitycontextconstraints"},
 			Verbs:         []string{"use"},
-		},
-		{
-			APIGroups: []string{""},
-			Resources: []string{"pods"},
-			Verbs:     []string{"create", "get", "list", "watch", "update", "patch", "delete"},
 		},
 	}
 	rbacResult, err := common_rbac.ReconcileRbac(ctx, helper, instance, rbacRules)
